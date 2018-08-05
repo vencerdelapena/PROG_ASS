fc261cbffe607b95cb9fe0823d6f964082caaeb5
vencerdelapena <42106696+vencerdelapena@users.noreply.github.com>
2018-08-05 16:14:52 +0800

    Add files via upload

---
+++ Vencer Dela Peña (prog ass).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main()
+ {
+    int stepnum;
+    cout << "How to pray";
+    cin>> stepnum;
+    
+    switch(stepnum)
+    {
+        
+        case 1:
+        cout<<"Step 1. Bow your head";
+        break;
+        case 2:
+        cout<<"Step 2. Close your eyes";
+        break;
+        case 3:
+        cout<<"Step 3. Think about Jesus";
+        break;
+}
+    return 0;
+}
